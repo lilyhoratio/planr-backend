f44ce2edd3ef4beb404af1cdfb82c27f4a77f4aa
lilyhoratio <lilyhzhou@gmail.com>
2019-09-26 02:52:42 -0700

    remove docs from github

--- docs/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "planr-backend",  "version": "1.0.0",  "description": "API documentation for Planr backend",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-09-26T09:46:09.308Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
