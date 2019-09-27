a9756f33e8b3b4238d7fb90111127cfc72691801
lilyhoratio <lilyhzhou@gmail.com>
2019-09-26 19:01:45 -0700

    moved apidocs to regular dependency for hosting

--- api/metrics/metrics-model.js
+++ api/metrics/metrics-model.js
@@ -1,32 +1,32 @@
-// // average budget across events - fix later
-router.get(`/analytics`, (req, res) => {
-  Events.getEventsAnalytics()
-    .then(eventsAnalytics => {
-      res.status(200).json(eventsAnalytics);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        message: "Error occurred while getting events' analytics.",
-        err
-      });
-    });
-});
+// // // average budget across events - fix later
+// router.get(`/analytics`, (req, res) => {
+//   Events.getEventsAnalytics()
+//     .then(eventsAnalytics => {
+//       res.status(200).json(eventsAnalytics);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({
+//         message: "Error occurred while getting events' analytics.",
+//         err
+//       });
+//     });
+// });
 
-// ANALYTICS - events-router
-router.get(`/:id/budget-items-cost`, (req, res) => {
-  const { id } = req.params;
+// // ANALYTICS - events-router
+// router.get(`/:id/budget-items-cost`, (req, res) => {
+//   const { id } = req.params;
 
-  Events.getBudgetItemsCostByEventId(id)
-    .then(cost => {
-      res.status(200).json(cost);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        message:
-          "Error occurred while getting total cost of budget items by event id",
-        err
-      });
-    });
-});
+//   Events.getBudgetItemsCostByEventId(id)
+//     .then(cost => {
+//       res.status(200).json(cost);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({
+//         message:
+//           "Error occurred while getting total cost of budget items by event id",
+//         err
+//       });
+//     });
+// });
