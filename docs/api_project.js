0b6fe8e02f7cb260effa4d090562a4536acfeee3
lilyhoratio <lilyhzhou@gmail.com>
2019-10-03 21:42:45 -0700

    adding docs

---
+++ docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "planr-backend",  "version": "1.0.0",  "description": "API documentation for Planr backend",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-09-26T11:16:10.000Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
